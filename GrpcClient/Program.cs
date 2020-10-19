d52b044cc09640c65c44c63ae454b897f26370cf
asfu1011 <asfu101@hs-karlsruhe.de>
2020-10-19 15:08:42 +0200

    Projektdateien hinzufügen.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest { Name = "Tim" };
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            /*var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+            */
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var lagerClient = new Lager.LagerClient(channel);
+            
+            var artikelRequested = new ArtikelSuchenMitIdModell { Id = "1" };
+
+            var artikel = await lagerClient.GetArtikelInfoAsync(artikelRequested);
+
+            Console.WriteLine($" ID : {artikel.Id} \n Name : {artikel.Name} \n Anzahl : {artikel.Anzahl} \n Ausverkauft : {artikel.IstAusverkauft}");
+
+
+           /* Console.WriteLine($"{artikel.id} {artikel.LastName}");
+            Console.WriteLine("Net Customer List");
+
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while( await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+                    Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName} : {currentCustomer.EmailAddress}");
+                }
+            }*/
+
+                Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/LagerService.cs
@@ -0,0 +1,76 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class LagerService : Lager.LagerBase
+    {
+        private readonly ILogger<LagerService> _logger;
+
+        public LagerService(ILogger<LagerService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<ArtikelModell> GetArtikelInfo(ArtikelSuchenMitIdModell request, ServerCallContext context)
+        {
+            ArtikelModell output = new ArtikelModell();
+            if (request.Id == "1")
+            {
+                output.Id = "1";
+                output.Name= "Stuhl";
+                output.Anzahl = 10;
+                output.MinBestand = 5;
+            }
+            else if (request.Id == "2")
+            {
+                output.Id = "2";
+                output.Name = "Tisch";
+                output.Anzahl = 25;
+                output.MinBestand = 3;
+
+            }
+            else
+            {
+                output.Id = "3";
+                output.Name = "Schrank";
+                output.Anzahl = 42;
+                output.MinBestand = 12;
+            }
+
+            return Task.FromResult(output);
+        }
+
+        /*public override async Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FirstName = "Furkan",
+                    LastName = "Asani",
+                    EmailAddress = "asfu1011@hs-karlsruhe.de",
+                    Age = 21,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Erkan",
+                    LastName = "Asani",
+                    EmailAddress = "aser1011@hs-karlsruhe.de",
+                    Age = 19,
+                    IsAlive = true
+                },
+
+            };
+            foreach (var cust in customers)
+            {
+                await Task.Delay(1000);
+                responseStream.WriteAsync(cust);
+            }
+        }*/
+    }
+}
